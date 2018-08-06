a81b3231592a2214e1351724229ef7b19e31db63
Cameron Reikes <cameronreikes@gmail.com>
2018-08-06 03:03:29 -0700

    Add ability to run multiple unit tests

--- src/tests.test.h
+++ src/tests.test.h
@@ -1,13 +1,15 @@
 #ifndef H_TESTS_TEST
 #define H_TESTS_TEST
 
-#include "greatest.h"
+#include "greatest/greatest.h"
 
 #include "main.test.h"
+#include "flags.test.h"
 
 extern SUITE(main_test);
 
-#define RUN_ALL_SUITS() \
-    RUN_SUITE(main_test);
+#define RUN_ALL_SUITS()   \
+    RUN_SUITE(main_test); \
+    RUN_SUITE(flags_test);
 
 #endif // H_TESTS_TEST
